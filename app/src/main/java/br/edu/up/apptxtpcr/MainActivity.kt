bb0e5b1e4f895f9c6a0d4f0f7bd8c7faf671ae98
Ygor <ygorespada14@gmail.com>
2023-08-17 10:21:18 -0300

    Alterações no App

--- app/src/main/java/br/edu/up/apptxtpcr/MainActivity.kt
+++ app/src/main/java/br/edu/up/apptxtpcr/MainActivity.kt
@@ -26,19 +26,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnProcessar.setOnClickListener(this)
     }
 
-    fun subtrair(a: Int, b: Int): Int{
+    fun subtrair(a: Int, b: Int): Int {
         return a - b
     }
 
-    fun somar(a: Int, b: Int): Int{
+    fun somar(a: Int, b: Int): Int {
         return a + b
     }
 
-    fun dividir(a: Int, b: Int): Int{
+    fun dividir(a: Int, b: Int): Int {
         return a / b
     }
 
-    fun multiplicar(a: Int, b: Int): Int{
+    fun multiplicar(a: Int, b: Int): Int {
         return a * b
     }
 
@@ -48,11 +48,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         var b = txt02.text.toString()
 
         //var texto = ""
-       //for(contador in 1..a.toInt()){
-            //texto += contador.toString() + "\n"
-            //texto += "Número: " + contador.toString() + "\n"
-            //texto += "Número: $contador \n"
-       //}
+        //for(contador in 1..a.toInt()){
+        //texto += contador.toString() + "\n"
+        //texto += "Número: " + contador.toString() + "\n"
+        //texto += "Número: $contador \n"
+        //}
 
         //var texto = ""
         //if (a.toInt() < 10){
@@ -72,25 +72,65 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //        }
 
 
+        //val lista = criarListaDeTexto(a.toInt(), b)
+        //var texto = converterParaTextoConsole(lista)
+        //txtConsole.text = texto //resultado.toString() textoCompleto
+        // }
 
-        val lista = criarListaDeTexto(a.toInt(), b)
-        var texto = converterParaTextoConsole(lista)
-        txtConsole.text = texto //resultado.toString() textoCompleto
-    }
+        val lista = criarListaDePessoas(a.toInt(), b)
+        txtConsole.text = converterPessoasParaTextoConsole(lista)
 
-    fun converterParaTextoConsole(lista: MutableList<String>): String{
-        var texto = ""
-        for(item: in lista){
-            texto += item + "\n"
-        }
-        return texto
-    }
-
-        fun criarListaDeTexto(total: Int, texto: String): MutableList<String> {
-            var lista = mutableListOf<String>()
-            for(contador in 1..total){
-                lista.add("$texto $contador")
+        fun converterParaTextoConsole(lista: MutableList<String>): String {
+            var texto = ""
+            for (item in lista) {
+                texto += item + "\n"
             }
-            return lista
+            return texto
+        }
+
+        fun converterPessoasParaTextoConsole(lista: MutableList<Pessoa>): String {
+            var texto = ""
+            for(pessoa in lista){
+                texto += "${pessoa.nome}, Idade: ${pessoa.idade} \n"
+            }
+            return texto
         }
     }
+
+
+    fun criarListaDeTexto(total: Int, texto: String): MutableList<String> {
+        var lista = mutableListOf<String>()
+        for (contador in 1..total) {
+            lista.add("$texto $contador")
+        }
+        return lista
+    }
+
+    //    class Pessoa {
+    //        lateinit var nome: String
+    //        fun calcular() {}
+    //
+    //    }
+
+    class Pessoa(val nome: String, val idade: Int)
+    fun criarListaDePessoas(total: Int, texto: String): MutableList<Pessoa> {
+        var p1 = Pessoa( "Ygor",  19)
+        var p2 = Pessoa( "Matheus",  20)
+
+        //Quando ja se conhece os objetos
+        //e se utiliza os objetos para criar a lista preenchida:
+        //var lista = mutableListOf(p1, p2)
+
+        //Quando ainda não se tem os obejetos
+        //e vai adiciona-los posteriormente:
+        var lista = mutableListOf<Pessoa>()
+        //lista.add(p1)
+        //lista.add(p2)
+        for(contador in 1..total){
+            //val pessoa = Pessoa("$texto $contador", contador)
+            //lista.add(pessoa)
+            lista.add(Pessoa("$texto $contador", contador))
+        }
+        return lista
+    }
+}
